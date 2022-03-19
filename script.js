ed3e0a6e686c4df652c159d62b81fe5e2311fb88
janaina <janainasantos89@hotmail.com>
2022-03-19 15:35:05 -0300

    Update

---
+++ script.js
@@ -0,0 +1,33 @@
+let titleIdReference = document.querySelector('#titleId');
+let descriptionIdReference = document.querySelector('#descriptionId');
+let imageIdReference = document.querySelector('#imageId');
+let addButtonReference = document.querySelector('#addButton');
+
+let cardsReference = document.querySelector('#cards');
+
+let posts = []
+
+addButtonReference.addEventListener('click',function(event){
+    
+    let titleIdReference = document.querySelector('#titleId').value;
+    let descriptionIdReference = document.querySelector('#descriptionId').value;
+    let imageIdReference = document.querySelector('#imageId').value;
+
+    posts.push ({
+        titleId: titleIdReference,
+        descriptionId: descriptionIdReference,
+        imageId: imageIdReference
+    })
+    event.preventDefault()
+    console.log(posts)
+
+    for(let post of posts){
+        cardsReference.innerHTML+=`
+        <div class="item">
+                <img src="${post.imageId}">
+                <h2> ${post.titleId}</h2>
+                <p>${post.descriptionId}</p>
+            </div>
+        `
+    }
+})
